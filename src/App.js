f697effd02205a0f577da9f9987a08801898b0e2
h662 <godh662@gmail.com>
2020-01-14 15:39:33 +0900

    ch1 Create Button fin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+import Button from "./Button";
+
 const App = () => {
   return (
     <>
       <h1>Hello React!</h1>
+      <Button
+        href="https://github.com/h662/zap-styled-components"
+        target="_blank"
+        rel="noopener"
+      >
+        myGitHub
+      </Button>
     </>
   );
 };
---
+++ src/Button.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Button = styled.a`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 7rem;
+  background: #449d44;
+  border: 2px solid #398439;
+  text-align: center;
+  color: #ffffff;
+`;
+
+export default Button;
